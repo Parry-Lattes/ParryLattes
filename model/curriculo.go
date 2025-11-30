b5519b9853a19e2c575bc0a5719695a150086e8a
202440601015 <emanuel.franco@unifesspa.edu.br>
2025-11-30 19:17:03 -0300

    Ajuste

--- model/curriculo.go
+++ model/curriculo.go
@@ -2,7 +2,7 @@ package model
 
 type Curriculo struct {
 	IdCurriculo       int64       `json:"-"`
-	IdPessoa          int         `json:"id_lattes"`
+	IdPessoa          int         `json:"-"`
 	UltimaAtualizacao string      `json:"ultima_atualizacao"`
 	Producoes         []*Producao `json:"producoes"`
 }
